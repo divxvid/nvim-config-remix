5a8ac5cdc874ee7e3730e375d4a0ffbc6202c561
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-04-04 20:44:57 +0530

    fix plugin path issue

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-require 'lua.options'
-require 'lua.keymaps'
-require 'lua.autocmds'
-require 'lua.lazy'
+require '7empest.options'
+require '7empest.keymaps'
+require '7empest.autocmds'
+require '7empest.lazy'
