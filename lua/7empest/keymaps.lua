0ddd477e86859fffec44910519d97d9d107d71bc
divxvid <divyansh.shrivastava@students.iiit.ac.in>
2024-04-07 13:22:08 +0530

    Remove garbage + add keymap for code diagnostics

--- lua/7empest/keymaps.lua
+++ lua/7empest/keymaps.lua
@@ -27,5 +27,5 @@ keymap.set('n', '<ESC>', '<cmd>nohlsearch<CR>')
 -- Diagnostics keymaps
 keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-keymap.set('n', '<leader>em', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror [M]essage' })
+keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { desc = 'Show [C]ode [D]iagnotics' })
 keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
