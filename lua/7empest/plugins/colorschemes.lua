1c3a5ba8eb830d53ff0ea207d8a1009a191afcc0
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2025-11-25 12:31:32 +0530

    Add solarized osaka theme

--- lua/7empest/plugins/colorschemes.lua
+++ lua/7empest/plugins/colorschemes.lua
@@ -186,27 +186,86 @@ return {
   --   end,
   -- },
   {
-    'blazkowolf/gruber-darker.nvim',
+    'craftzdog/solarized-osaka.nvim',
+    lazy = false,
+    priority = 1000,
     opts = {
-      bold = true,
-      invert = {
-        signs = false,
-        tabline = false,
-        visual = false,
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      transparent = true, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = 'dark', -- style for sidebars, see below
+        floats = 'dark', -- style for floating windows
       },
-      italic = {
-        strings = false,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      undercurl = true,
-      underline = true,
+      sidebars = { 'qf', 'help' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+      dim_inactive = false, -- dims inactive windows
+      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+      --- You can override specific color groups to use other groups or a hex color
+      --- function will be called with a ColorScheme table
+      ---@param colors ColorScheme
+      on_colors = function(colors) end,
+
+      --- You can override specific highlights to use other groups or a hex color
+      --- function will be called with a Highlights and ColorScheme table
+      ---@param highlights Highlights
+      ---@param colors ColorScheme
+      on_highlights = function(highlights, colors) end,
     },
     init = function()
-      vim.cmd.colorscheme 'gruber-darker'
+      vim.cmd.colorscheme 'solarized-osaka'
     end,
   },
+  -- {
+  --   'svrana/neosolarized.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('neosolarized').setup {
+  --       comment_italics = true,
+  --       background_set = true,
+  --     }
+  --     vim.cmd.colorscheme 'neosolarized'
+  --   end,
+  --   dependencies = {
+  --     'tjdevries/colorbuddy.nvim',
+  --   },
+  --   -- init = function()
+  --   --   vim.cmd.colorscheme 'neosolarized'
+  --   -- end,
+  -- },
+  -- {
+  --   'blazkowolf/gruber-darker.nvim',
+  --   opts = {
+  --     bold = true,
+  --     invert = {
+  --       signs = false,
+  --       tabline = false,
+  --       visual = false,
+  --     },
+  --     italic = {
+  --       strings = false,
+  --       comments = true,
+  --       operators = false,
+  --       folds = true,
+  --     },
+  --     undercurl = true,
+  --     underline = true,
+  --   },
+  --   init = function()
+  --     vim.cmd.colorscheme 'gruber-darker'
+  --   end,
+  -- },
   -- {
   --   'ellisonleao/gruvbox.nvim',
   --   lazy = false,
